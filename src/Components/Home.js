d96b0118515ef4261cfebbdccef28414840e4500
Ejim Michael <ejimmichael1@gmail.com>
2022-07-21 23:17:51 +0100

    Install react-icons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { auth, fs } from '../FirebaseConfig/Firebase';
 import NavBar from './NavBar';
 import Products from './Products';
 
 function Home() {
+  // getting current user uid
+  function GetUserUid() {
+    const [uid, setUid] = useState(null);
+    useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          setUid(user.uid);
+        }
+      });
+    }, []);
+    return uid;
+  }
+
+  const uid = GetUserUid();
+
+  // getting current user function
+  function GetCurrentUser() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          fs.collection("users")
+            .doc(user.uid)
+            .get()
+            .then((snapshot) => {
+              setUser(snapshot.data().FullName);
+            });
+        } else {
+          setUser(null);
+        }
+      });
+    }, []);
+    return user;
+  }
+
+  const user = GetCurrentUser();
   return (
     <>
-    <NavBar />
-    <Products />
+      <NavBar user={user} />
+      <Products />
     </>
-  )
+  );
 }
 
 export default Home;
