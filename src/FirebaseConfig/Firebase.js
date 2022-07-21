9d34661c08d9b27389d68bf3e2ec96573a46696d
Ejim Michael <ejimmichael1@gmail.com>
2022-07-21 21:40:08 +0100

    setup firebase

---
+++ src/FirebaseConfig/Firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase"; 
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  // apiKey: "AIzaSyAey3GSdSUwL-xOBmCzAn0vScYv2uw8ZzE",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "catfish-e-commerce.firebaseapp.com",
+  projectId: "catfish-e-commerce",
+  storageBucket: "catfish-e-commerce.appspot.com",
+  messagingSenderId: "115502660683",
+  appId: "1:115502660683:web:60d47f2ebef30390ca21ce",
+  measurementId: "G-8XQYMXE1WZ",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const fs = firebase.firestore();
+const storage = firebase.storage();
+
+export { auth, fs, storage };
