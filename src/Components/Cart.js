118d6aae7d6fa625339e959621d8d2654d2f80e6
Ejim Michael <ejimmichael1@gmail.com>
2022-07-22 17:49:20 +0100

    Getting current user

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { auth, fs } from "../FirebaseConfig/Firebase";
+import NavBar from "./NavBar";
 
 function Cart() {
+
+  function GetCurrentUser() {
+    const [user, setUser] = useState(null);
+
+    
+    useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          fs.collection("users")
+            .doc(user.uid)
+            .get()
+            .then((snapshot) => {
+              setUser(snapshot.data().FullName);
+            });
+        } else {
+          setUser(null);
+        }
+      });
+    }, []);
+    return user;
+  }
   return (
-    <div>Cart</div>
-  )
+    <>
+      <NavBar user={user} totalProducts={totalProducts} />
+    </>
+  );
 }
 
 export default Cart;
