b43faec4f063cb0a346cdb836f0559151e43b5fc
Ejim Michael <ejimmichael1@gmail.com>
2022-07-22 18:00:52 +0100

    get Cart product from firestore

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,11 +3,9 @@ import { auth, fs } from "../FirebaseConfig/Firebase";
 import NavBar from "./NavBar";
 
 function Cart() {
-
   function GetCurrentUser() {
     const [user, setUser] = useState(null);
 
-    
     useEffect(() => {
       auth.onAuthStateChanged((user) => {
         if (user) {
@@ -24,9 +22,34 @@ function Cart() {
     }, []);
     return user;
   }
+
+  const user = GetCurrentUser();
+
+  // state of cart products
+  const [cartProducts, setCartProducts] = useState([]);
+
+  // getting cart products from firestore collection and updating the state
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        fs.collection("Cart " + user.uid).onSnapshot((snapshot) => {
+          const newCartProduct = snapshot.docs.map((doc) => ({
+            ID: doc.id,
+            ...doc.data(),
+          }));
+          setCartProducts(newCartProduct);
+        });
+      } else {
+        console.log("user is not signed in to retrieve cart");
+      }
+    });
+  }, []);
+
+  // console.log(cartProducts);
+
   return (
     <>
-      <NavBar user={user} totalProducts={totalProducts} />
+      <NavBar user={user} />
     </>
   );
 }
